56b6ab4e56dbec138d19494616175c127c02980f
Stanislav Rakitov <prosperousrf@gmail.com>
2022-05-08 14:52:16 +0300

    Задача 2. Межпоточный диалог со счетчиком

---
+++ src/MyCallable.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+/**
+ * @author Stanislav Rakitov
+ */
+public class MyCallable implements Callable<Integer> {
+
+  private Integer count;
+  private final String name;
+
+  public MyCallable(String name) {
+    this.name = name;
+    this.count = 0;
+  }
+
+  @Override
+  public Integer call() throws Exception {
+
+    Random random = new Random();
+
+    /* каждый поток будет пробегать от 1 до 20 раз */
+    int anInt = random.nextInt(20);
+    for (int i = 0; i < anInt; i++) {
+      System.out.printf("Я поток %s. Всем привет! Я на %d круге\n", name, i);
+      count++;
+
+      /* Решение для задержки потока на случайное время в диапазоне от 1 до 2.5 секунд */
+      int sleepTimer = random.ints(1000, 2501).findFirst().orElse(3000);
+      Thread.sleep(sleepTimer);
+    }
+    return count;
+  }
+}
